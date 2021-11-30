4057e48f58c55242333467c654bbdab4b4ac533a
HenriqueTrigolo <henriquetrigolo@hotmail.com>
2021-11-30 18:11:14 -0300

    criação dos testes do estacionamento

---
+++ testes/teste.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <title> Nosso singelo formulário </title>
+        <meta charset="utf-8">
+    </head>
+    <body>
+
+        <form action="script.php" method="get">
+            Campo 1: <input type=text name=campo1><br>
+            Campo 2: <input type=text name=campo2><br>
+            <input type=submit value="OK">
+        </form>
+
+    </body>
+</html>
